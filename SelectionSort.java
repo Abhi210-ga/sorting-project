a42431702f9214c62a53a67f14c41db928823ff6
Abhi210-ga <ag4481@pleasantonusd.net>
2025-01-29 13:55:37 -0800

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,21 @@
 public class SelectionSort implements Sorter {
 
+    @Override
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+
+        int n = input.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = input[minIndex];
+            input[minIndex] = input[i];
+            input[i] = temp;
+        }
     }
 }
